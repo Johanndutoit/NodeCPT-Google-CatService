1476ce0f7ba34b221e4b433a1818f7094e1c5e58
Johann du Toit <exxonno@gmail.com>
2014-07-29 01:09:49 +0200

    Removed npm modules

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-var express = require('express');
-
-var app = express();
-
-app.get('/', function(req, res) {
-
-res.send('<center style="margin: 90px auto;"><h2>Your Daily Cat Image</h2><img src="http://theoldreader.com/kittens/600/400" /></center>');
-
-});
-
-app.listen(3000, function(){ console.log('Running on port 3000'); });
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<center style="margin: 90px auto;"><h2>Your Daily Cat Image</h2><img src="http://theoldreader.com/kittens/600/400" /></center>');
+}).listen(3000, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:3000/');
